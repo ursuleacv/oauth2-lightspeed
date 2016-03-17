81dbfa4af447bef0668d4bdbb54d1a3d7dc182ee
Valentin Ursuleac <ursuleacv@gmail.com>
2016-03-17 16:53:39 -0400

    Adding resource owner

---
+++ src/Provider/LightspeedResourceOwner.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace League\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+
+class LightspeedResourceOwner implements ResourceOwnerInterface
+{
+    /**
+     * Raw response
+     *
+     * @var array
+     */
+    protected $response;
+
+    /**
+     * Creates new resource owner.
+     *
+     * @param array  $response
+     */
+    public function __construct(array $response = array())
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Get Account ID
+     *
+     * @return string|null
+     */
+    public function getId()
+    {
+        return $this->getResponseData('Account.accountID');
+    }
+
+    /**
+     * Attempts to pull value from array using dot notation.
+     *
+     * @param string $path
+     * @param string $default
+     *
+     * @return mixed
+     */
+    protected function getResponseData($path, $default = null)
+    {
+        $array = $this->response;
+        if (!empty($path)) {
+            $keys = explode('.', $path);
+            foreach ($keys as $key) {
+                if (isset($array[$key])) {
+                    $array = $array[$key];
+                } else {
+                    return $default;
+                }
+            }
+        }
+        return $array;
+    }
+
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
+}
